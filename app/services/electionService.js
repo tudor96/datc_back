c61b4615a90e47b777c72f4f27f1b77fac9deba9
unknown <valentingorcea@gmail.com>
2020-01-05 01:36:03 +0200

    tudor za master

--- app/services/electionService.js
+++ app/services/electionService.js
@@ -19,14 +19,13 @@ ElectionService.prototype.getPolls = async function () {
           sql = `SELECT id, name from question, pollquestion where pollId = ? and id = questionId`;
           let responseQuestions = await this._dbService.query(sql, [poll.id]);
           poll["questions"] = responseQuestions;
-          return Promise.all(poll.questions.map(async (question, index) => {
+          await Promise.all(poll.questions.map(async (question, index) => {
             sql = 'SELECT o.id, o.name, o.description, p.name as partidName from `option` o, partid p where questionId = ? and o.idPartid = p.id';
             let responseOptions = await this._dbService.query(sql, [question.id]);
             question["options"] = responseOptions;
-            return Promise.resolve(poll);
           }))
+          return Promise.resolve(poll);
         }))
-
         return resolve(final);
       }
       else {
@@ -66,12 +65,12 @@ ElectionService.prototype.getPoll = async function (id) {
           sql = `SELECT id, name from question, pollquestion where pollId = ? and id = questionId`;
           let responseQuestions = await this._dbService.query(sql, [poll.id]);
           poll["questions"] = responseQuestions;
-          return Promise.all(poll.questions.map(async question => {
+          await Promise.all(poll.questions.map(async question => {
             sql = 'SELECT o.id, o.name, o.description, p.name as partidName from `option` o, partid p where questionId = ? and o.idPartid = p.id';
             let responseOptions = await this._dbService.query(sql, [question.id]);
             question["options"] = responseOptions;
-            return Promise.resolve(poll);
           }))
+          return Promise.resolve(poll);
         }))
 
         return resolve(final);
