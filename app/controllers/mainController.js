04cf2edf5d3022d584831850779682f7bc14b018
unknown <valentingorcea@gmail.com>
2020-01-09 00:06:34 +0200

    yolo change again

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -27,9 +27,9 @@ module.exports = function (log, dbService, config, userService) {
     // =============================================================================
     //app.use(express.static('build'));
 	
-	app.use(express.static(path.join(__dirname, 'build')));
+	app.use(express.static(path.join(__dirname, '../../build')));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
     // authentication
